abc5f2406d915c6bd178d2866e573a9c3ea08ea3
Tayssir_Lahbib <tayssirlahbib2003@gmail.com>
2024-01-22 18:08:08 +0100

    create event and family entities

---
+++ src/Entity/Family.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FamilyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FamilyRepository::class)]
+class Family
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $familyName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFamilyName(): ?string
+    {
+        return $this->familyName;
+    }
+
+    public function setFamilyName(string $familyName): static
+    {
+        $this->familyName = $familyName;
+
+        return $this;
+    }
+}
