bd8310a63a683f2b8a7c7800022b2b0c6eb3b2c0
Iskander Abbassi <57728067+iskanderAB@users.noreply.github.com>
2024-10-22 22:05:39 +0100

    delete comments

--- src/Security/SecurityAuthenticator.php
+++ src/Security/SecurityAuthenticator.php
@@ -47,9 +47,7 @@ class SecurityAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
         return new RedirectResponse($this->urlGenerator->generate('app_family_index'));
-        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
